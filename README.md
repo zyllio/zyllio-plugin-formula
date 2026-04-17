6ab1b0f9832d46086ba99a5ec5e207b9e7fc25d2
Zyllio <96659995+zyllio@users.noreply.github.com>
2026-04-17 20:35:31 +0200

    -

---
+++ README.md
@@ -0,0 +1,22 @@
+# Zyllio Plugin Area Formula
+
+This Zyllio plugin implements a simple Area calculation Formula
+
+
+# Experiment
+
+Install this pre-built file to experiment the plugin:
+```
+https://zyllio.github.io/zyllio-plugin-formula/plugin.js
+```
+
+# Fork this plugin
+
+You are welcome to clone and fork this repository, looking forward your creativity 😀
+
+# Support
+
+Zyllio team is available for any support, feature requests and questions
+
+- email: contact@zyllio.com
+- twitter: [@zyllio](https://twitter.com/zyllio) 
---
+++ plugin.js
@@ -0,0 +1,54 @@
+(function () {
+
+  class AreaFunction {
+    
+    execute() {
+      return this.width * this.height.toString() + 'm²';
+    }
+  }
+
+  const zyAreaFunction = new AreaFunction();
+
+  const IconData =
+    '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">' +
+    '<path d="M4 6H20V18H4V6M6 8V16H18V8H6Z" />' +
+    '</svg>';
+
+  const AreaMetadata = {
+    id: 'area',
+    metadataVersion: 2,
+    icon: IconData,
+    label: 'Area',
+    category: 'Number',
+    format: 'number',
+    properties: [
+      {
+        id: 'width',
+        name: 'Width',
+        type: 'number',
+        default: '0',
+        main: true
+      },
+      {
+        id: 'height',
+        name: 'Height',
+        type: 'number',
+        default: '0',
+        main: false
+      }
+    ],
+    translations: [
+      {
+        lang: 'fr',
+        label: 'Surface',
+        category: 'Nombre',
+        properties: [
+          { id: 'width', name: 'Largeur' },
+          { id: 'height', name: 'Hauteur' }
+        ]
+      }
+    ]
+  };
+
+  zySdk.services.registry.registerFunction(AreaMetadata, AreaFunction);
+})();
